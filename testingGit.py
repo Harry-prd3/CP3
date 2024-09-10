f48c321b45345774f1bd0651f51d0cda7b13b427
Harry-prd3 <harrison.baxter@ucas-edu.net>
2024-09-10 11:31:08 -0600

    added comment and print

--- testingGit.py
+++ testingGit.py
@@ -1,5 +1,7 @@
 print("this is for testing.")
 print("cahange teehee")
 
-
+# BE SURE THAT BOTH GIT AND VS LOOK IDENTICAl BEFORE YOU MAKE CHANGES
 print("now im on git!")
+
+print("now im on vs code")
