2ccb4f3290d770eec14d9486618dc381364586c1
Harry-prd3 <harrison.baxter@ucas-edu.net>
2024-10-09 11:43:07 -0600

    added notes on methods: class methods and static methods

--- notes.py
+++ notes.py
@@ -37,26 +37,82 @@ cat = Animal("Tom", "meow meow", 21, "no thanks", "Epic")
 frog = Animal("Jarrod", "Poisin Dart Frog", 500, "Feminane", "Legendary")
 
 
-print(cat)
-print(frog)
+#print(cat)
+#print(frog)
 
 # Creating the new argument for the fuction deaths
-cat.deaths = 0
-frog.deaths = 0
+#cat.deaths = 0
+#frog.deaths = 0
 
 # To call methods you put the name of the object.name of the method(arguments)
+"""""
 print("")
 print(cat.fight(frog))
 print(cat.deaths)
+"""""
 
 # Reasinging the name of the cat
 cat.name = "Thomas"
-
+"""""
 print("")
 print(cat.fight(frog))
 print(cat.deaths)
 print(frog.deaths)
-
+"""""
 # Deleating tom cause he is a stupid little baby
 cat = None
-print(cat)
+# print(cat)
+
+
+
+
+#Methods
+
+class pokemon:
+    def __init__(self, name, hp, typ, lvl):
+        self.name = name
+        self.hp = hp
+        self.type = typ
+        self.lvl = lvl
+    
+    def combat(self, other):
+        if self.lvl > other.lvl:
+            return(f"{self.name} yippee")
+        elif self.lvl < other.lvl:
+            return(f"aw {self.name} lost to {other.name}, hp is 0 now :(")
+        else:
+            return(f"both {self.name} and {other.name} dead. cry about it.")
+    
+    def __str__(self):
+        return(f""" 
+                Name: {self.name}
+                Type : {self.type}
+                Level: {self.lvl}
+                HP: {self.hp}
+""")
+    
+    # @classmethods make methods that can not affect instance variables of an object
+    def lvl_up(self):
+        self.lvl += 1
+        self.hp = int(self.hp *1.5)
+
+    @classmethod
+    def pikachu(self):
+        return(pokemon(input("give me a name: "), 50, "-eletric", -9999))
+    
+    #static methods do not require self or class
+    def hp_update(poke):
+        return poke.hp - 100
+
+
+
+
+
+eevee = pokemon("JayRod", 37, "normal", 2)
+electivire = pokemon("bobsive", 1, "electric", 36.872)
+pika = pokemon.pikachu()
+
+print(eevee.lvl_up)
+print(eevee)
+pika.hp = pokemon.hp_update(pika)
+print(pika)
