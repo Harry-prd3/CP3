7fdfac4cc8df3e1cadbcba1aa24badf92d69519c
Harry-prd3 <harrison.baxter@ucas-edu.net>
2024-10-29 11:49:01 -0600

    polymorphism started

---
+++ polymorphism.py
@@ -0,0 +1,47 @@
+class Contacts:
+     def __init__(self):
+         self.view = 'list'
+         self.contact_list = []
+         self.choice = None
+         self.index = None
+    
+     def show_list(self):
+        print()
+        if len(self.contact_list) == 0:
+            self.choice = input('(A)dd a new contact \n(Q)uit \n> ').lower()
+        else:
+            self.view = 'quit'
+        self.handle_choice()
+
+     def handle_choice(self):
+        if self.choice == 'q':
+            self.view = 'quit'
+        elif self.choice == 'a' and self.view == 'list':
+            self.view = 'add'
+
+     def display(self):
+         while True:
+             if self.view == 'list':
+              self.show_list()
+             elif self.view == 'info':
+                self.show_info()
+             elif self.view == 'add':
+                print()
+                self.add_contact()
+             elif self.view == 'quit':
+                 print('\nClosing the contact list...\n')
+                 break
+
+class Information:
+  def __init__(self):
+    self.first_name = input('Enter their first name: ')
+    self.last_name = input('Enter their last name: ')
+    self.personal_phone = input('Enter their personal phone number: ')
+    self.personal_email = input('Enter their personal email address: ')
+    self.work_phone = input('Enter their work phone number: ')
+    self.work_email = input('Enter their work email address: ')
+    self.title = input('Enter their work title: ')
+
+contacts = Contacts()
+contacts.display()
+    
